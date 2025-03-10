a91b39bb1e1529662fda84116dd4efa695ac0cc9
Sennaikee <sennaikeg@gmail.com>
2025-03-11 00:21:01 +0100

    Updated config.js again

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ const db = mysql.createConnection({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT, // Use DB_PORT
+  port: 3306, // MySQL always runs on 3306
 });
 
 db.connect((err) => {
